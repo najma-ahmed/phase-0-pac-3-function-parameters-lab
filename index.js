9ebc73a6e1432643fcf6d3d94002410b390b9b4e
najma-ahmed <najmahmed20162016@gmail.com>
2022-11-20 22:57:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+   return "Hi, my name is ${name}.";
+  }
+
+function introductionWithLanguage(name, language) {
+    console.log("Hi, my name is ${name} and I am learning to program in ${language}.");
+}
+
+function introductionWithLanguageOptional(name, language) {
+    console.log("Hi, my name is ${name} and I am learning to program in ${language}.");
+}
